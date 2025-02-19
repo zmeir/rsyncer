c5abb47c0a1a633684e32e07678e93932d3724da
Zohar Meir <33152084+zmeir@users.noreply.github.com>
2025-02-19 13:44:12 +0200

    Move all build stuff to pypproject and change build backend

--- rsyncer/__init__.py
+++ rsyncer/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import __title__, __description__, __url__, __version__
-from .__version__ import __author__, __author_email__, __copyright__
-
 from rsyncer import rsync
+
+__version__ = "0.0.1"
